95659e85582ee96a9fd1021ab74445fc6aee9f60
Weston Ruter <weston@xwp.co>
2016-05-03 22:01:17 -0700

    flesh out readme and add dev files

---
+++ Gruntfile.js
@@ -0,0 +1,162 @@
+/* eslint-env node */
+/* jshint node:true */
+
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// JavaScript linting with JSHint.
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'js/*.js',
+				'!js/*.min.js'
+			]
+		},
+
+		// Minify .js files.
+		uglify: {
+			options: {
+				preserveComments: false
+			},
+			core: {
+				files: [ {
+					expand: true,
+					cwd: 'js/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: 'js/',
+					ext: '.min.js'
+				} ]
+			}
+		},
+
+		// Minify .css files.
+		cssmin: {
+			core: {
+				files: [ {
+					expand: true,
+					cwd: 'css/',
+					src: [
+						'*.css',
+						'!*.min.css'
+					],
+					dest: 'css/',
+					ext: '.min.css'
+				} ]
+			}
+		},
+
+		// Build a deploy-able plugin
+		copy: {
+			build: {
+				src: [
+					'*.php',
+					'css/*',
+					'js/*',
+					'php/*',
+					'readme.txt'
+				],
+				dest: 'build',
+				expand: true,
+				dot: true
+			}
+		},
+
+		// Clean up the build
+		clean: {
+			build: {
+				src: [ 'build' ]
+			}
+		},
+
+		// VVV (Varying Vagrant Vagrants) Paths
+		vvv: {
+			'plugin': '/srv/www/wordpress-develop/src/wp-content/plugins/<%= pkg.name %>',
+			'coverage': '/srv/www/default/coverage/<%= pkg.name %>'
+		},
+
+		// Shell actions
+		shell: {
+			options: {
+				stdout: true,
+				stderr: true
+			},
+			readme: {
+				command: 'cd ./dev-lib && ./generate-markdown-readme' // Generate the readme.md
+			},
+			phpunit: {
+				command: 'vagrant ssh -c "cd <%= vvv.plugin %> && phpunit"'
+			},
+			phpunit_c: {
+				command: 'vagrant ssh -c "cd <%= vvv.plugin %> && phpunit --coverage-html <%= vvv.coverage %>"'
+			}
+		},
+
+		// Deploys a git Repo to the WordPress SVN repo
+		wp_deploy: {
+			deploy: {
+				options: {
+					plugin_slug: '<%= pkg.name %>',
+					build_dir: 'build',
+					assets_dir: 'wp-assets'
+				}
+			}
+		}
+
+	} );
+
+	// Load tasks
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-shell' );
+	grunt.loadNpmTasks( 'grunt-wp-deploy' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'jshint',
+		'uglify',
+		'cssmin'
+	] );
+
+	grunt.registerTask( 'readme', [
+		'shell:readme'
+	] );
+
+	grunt.registerTask( 'phpunit', [
+		'shell:phpunit'
+	] );
+
+	grunt.registerTask( 'phpunit_c', [
+		'shell:phpunit_c'
+	] );
+
+	grunt.registerTask( 'dev', [
+		'default',
+		'readme'
+	] );
+
+	grunt.registerTask( 'build', [
+		'default',
+		'readme',
+		'copy'
+	] );
+
+	grunt.registerTask( 'deploy', [
+		'build',
+		'wp_deploy',
+		'clean'
+	] );
+
+};
--- js-widgets.php
+++ js-widgets.php
@@ -1,7 +1,28 @@
 <?php
 /**
  * Plugin Name: JS Widgets
- * Description: The next iteration on widgets in Core, embracing JS for UI and powering the Widgets REST API. Codename: Widgets Next, aka Widgets 3.0.
+ * Description: The next generation of widgets in Core (Widgets 3.0), embracing JS for UI and powering the Widgets REST API.
+ * Plugin URI: https://github.com/xwp/wp-js-widgets/
+ * Version: 0.1.0
+ * Author: XWP
+ * Author URI: https://make.xwp.co/
+ * License: GPLv2+
+ *
+ * Copyright (c) 2016 XWP (https://xwp.co/)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * @package JSWidgets
  */
