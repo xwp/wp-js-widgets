cda8014f525fd95bb19bc0ce7d0a7b18cf6a042b
Weston Ruter <weston@xwp.co>
2016-12-24 17:55:16 -0800

    Add categories widget

---
+++ core-adapter-widgets/categories/class.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Class WP_JS_Widget_Categories.
+ *
+ * @package JS_Widgets
+ */
+
+/**
+ * Class WP_JS_Widget_Categories
+ *
+ * @package JS_Widgets
+ */
+class WP_JS_Widget_Categories extends WP_Adapter_JS_Widget {
+
+	/**
+	 * WP_JS_Widget_Categories constructor.
+	 *
+	 * @param JS_Widgets_Plugin    $plugin         Plugin.
+	 * @param WP_Widget_Categories $adapted_widget Adapted/wrapped core widget.
+	 */
+	public function __construct( JS_Widgets_Plugin $plugin, WP_Widget_Categories $adapted_widget ) {
+		parent::__construct( $plugin, $adapted_widget );
+	}
+
+	/**
+	 * Get instance schema properties.
+	 *
+	 * @return array Schema.
+	 */
+	public function get_item_schema() {
+		$schema = array_merge(
+			parent::get_item_schema(),
+			array(
+				'dropdown' => array(
+					'description' => __( 'Display as dropdown', 'default' ),
+					'type' => 'boolean',
+					'default' => false,
+					'context' => array( 'view', 'edit', 'embed' ),
+					'arg_options' => array(
+						'validate_callback' => 'rest_validate_request_arg',
+					),
+				),
+				'count' => array(
+					'description' => __( 'Show post counts', 'default' ),
+					'type' => 'boolean',
+					'default' => false,
+					'context' => array( 'view', 'edit', 'embed' ),
+					'arg_options' => array(
+						'validate_callback' => 'rest_validate_request_arg',
+					),
+				),
+				'hierarchical' => array(
+					'description' => __( 'Show hierarchy', 'default' ),
+					'type' => 'boolean',
+					'default' => false,
+					'context' => array( 'view', 'edit', 'embed' ),
+					'arg_options' => array(
+						'validate_callback' => 'rest_validate_request_arg',
+					),
+				),
+			)
+		);
+		return $schema;
+	}
+
+	/**
+	 * Render JS Template.
+	 */
+	public function form_template() {
+		$item_schema = $this->get_item_schema();
+		?>
+		<script id="tmpl-customize-widget-form-<?php echo esc_attr( $this->id_base ) ?>" type="text/template">
+			<?php
+			$this->render_title_form_field( array(
+				'placeholder' => $item_schema['title']['properties']['raw']['default'],
+			) );
+			$this->render_form_field( array(
+				'name' => 'dropdown',
+				'label' => __( 'Display as dropdown', 'default' ),
+				'type' => 'checkbox',
+			) );
+			$this->render_form_field( array(
+				'name' => 'count',
+				'label' => __( 'Show post counts', 'default' ),
+				'type' => 'checkbox',
+			) );
+			$this->render_form_field( array(
+				'name' => 'hierarchical',
+				'label' => __( 'Show hierarchy', 'default' ),
+				'type' => 'checkbox',
+			) );
+			?>
+		</script>
+		<?php
+	}
+}
---
+++ core-adapter-widgets/categories/form.js
@@ -0,0 +1,23 @@
+/* global wp, module */
+/* eslint consistent-this: [ "error", "form" ] */
+/* eslint-disable strict */
+/* eslint-disable complexity */
+
+wp.customize.Widgets.formConstructor.categories = (function( api ) {
+	'use strict';
+
+	var CategoriesWidgetForm;
+
+	/**
+	 * Text Widget Form.
+	 *
+	 * @constructor
+	 */
+	CategoriesWidgetForm = api.Widgets.CoreForm.extend( {} );
+
+	if ( 'undefined' !== typeof module ) {
+		module.exports = CategoriesWidgetForm;
+	}
+	return CategoriesWidgetForm;
+
+})( wp.customize );
