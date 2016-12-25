ae8deca2133132f430693c11e70c5f3d8f34a74d
Weston Ruter <weston@xwp.co>
2016-12-24 18:09:42 -0800

    Add meta widget

---
+++ core-adapter-widgets/meta/class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class WP_JS_Widget_Meta.
+ *
+ * @package JS_Widgets
+ */
+
+/**
+ * Class WP_JS_Widget_Text
+ *
+ * @package WP_JS_Widget_Meta
+ */
+class WP_JS_Widget_Meta extends WP_Adapter_JS_Widget {
+
+	/**
+	 * WP_JS_Widget_Meta constructor.
+	 *
+	 * @param JS_Widgets_Plugin $plugin         Plugin.
+	 * @param WP_Widget_Meta    $adapted_widget Adapted/wrapped core widget.
+	 */
+	public function __construct( JS_Widgets_Plugin $plugin, WP_Widget_Meta $adapted_widget ) {
+		parent::__construct( $plugin, $adapted_widget );
+	}
+}
---
+++ core-adapter-widgets/meta/form.js
@@ -0,0 +1,23 @@
+/* global wp, module */
+/* eslint consistent-this: [ "error", "form" ] */
+/* eslint-disable strict */
+/* eslint-disable complexity */
+
+wp.customize.Widgets.formConstructor.meta = (function() {
+	'use strict';
+
+	var MetaWidgetForm;
+
+	/**
+	 * Calendar Widget Form.
+	 *
+	 * @constructor
+	 */
+	MetaWidgetForm = wp.customize.Widgets.CoreForm.extend( {} );
+
+	if ( 'undefined' !== typeof module ) {
+		module.exports = MetaWidgetForm;
+	}
+	return MetaWidgetForm;
+
+})();
