f68c34e8d272aebf2b242a1d2cdac92f25632881
Weston Ruter <weston@xwp.co>
2017-01-01 21:19:43 -0800

    Return terms in REST response for categories widget

--- core-adapter-widgets/categories/class.php
+++ core-adapter-widgets/categories/class.php
@@ -58,10 +58,143 @@ class WP_JS_Widget_Categories extends WP_Adapter_JS_Widget {
 						'validate_callback' => 'rest_validate_request_arg',
 					),
 				),
-				// @todo Add categories readonly response that lists the categories that would be returned.
+				'terms' => array(
+					'description' => __( 'The IDs for the category terms.', 'js-widgets' ),
+					'type' => 'array',
+					'items' => array(
+						'type' => 'integer',
+					),
+					'context' => array( 'view', 'edit', 'embed' ),
+					'readonly' => true, // @todo This is not causing an arg to be rejected.
+					'default' => array(),
+				),
 			)
 		);
 		return $schema;
+	}
+
+	/**
+	 * Render a widget instance for a REST API response.
+	 *
+	 * @inheritdoc
+	 *
+	 * @param array           $instance Raw database instance.
+	 * @param WP_REST_Request $request  REST request.
+	 * @return array Widget item.
+	 */
+	public function prepare_item_for_response( $instance, $request ) {
+		$item = parent::prepare_item_for_response( $instance, $request );
+
+		$cat_args = array(
+			'orderby' => 'name',
+			'show_count' => $instance['count'],
+			'hierarchical' => $instance['hierarchical'],
+		);
+
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-categories.php */
+		$cat_args = apply_filters( 'widget_categories_args', $cat_args );
+
+		$item['terms'] = wp_list_pluck( $this->get_categories_list( $cat_args ), 'term_id' );
+
+		return $item;
+	}
+
+
+	/**
+	 * Get categories list.
+	 *
+	 * Adaptation of `wp_list_categories()` to return raw (unrendered) data.
+	 *
+	 * @see wp_list_categories()
+	 *
+	 * @param array $args Args.
+	 * @return array|false Categories list or false on failure.
+	 */
+	public function get_categories_list( $args ) {
+		$defaults = array(
+			'child_of'            => 0,
+			'current_category'    => 0,
+			'depth'               => 0,
+			'echo'                => 1,
+			'exclude'             => '',
+			'exclude_tree'        => '',
+			'feed'                => '',
+			'feed_image'          => '',
+			'feed_type'           => '',
+			'hide_empty'          => 1,
+			'hierarchical'        => true,
+			'order'               => 'ASC',
+			'orderby'             => 'name',
+			'style'               => 'list',
+			'taxonomy'            => 'category',
+			'use_desc_for_title'  => 1,
+		);
+
+		$r = wp_parse_args( $args, $defaults );
+
+		if ( ! isset( $r['pad_counts'] ) && $r['show_count'] && $r['hierarchical'] ) {
+			$r['pad_counts'] = true;
+		}
+
+		// Descendants of exclusions should be excluded too.
+		if ( true == $r['hierarchical'] ) {
+			$exclude_tree = array();
+
+			if ( $r['exclude_tree'] ) {
+				$exclude_tree = array_merge( $exclude_tree, wp_parse_id_list( $r['exclude_tree'] ) );
+			}
+
+			if ( $r['exclude'] ) {
+				$exclude_tree = array_merge( $exclude_tree, wp_parse_id_list( $r['exclude'] ) );
+			}
+
+			$r['exclude_tree'] = $exclude_tree;
+			$r['exclude'] = '';
+		}
+
+		if ( ! isset( $r['class'] ) ) {
+			$r['class'] = ( 'category' == $r['taxonomy'] ) ? 'categories' : $r['taxonomy'];
+		}
+
+		if ( ! taxonomy_exists( $r['taxonomy'] ) ) {
+			return false;
+		}
+
+		return get_categories( $r );
+	}
+
+	/**
+	 * Prepare links for the response.
+	 *
+	 * @param WP_REST_Response           $response   Response.
+	 * @param WP_REST_Request            $request    Request.
+	 * @param JS_Widgets_REST_Controller $controller Controller.
+	 * @return array Links for the given post.
+	 */
+	public function get_rest_response_links( $response, $request, $controller ) {
+		$links = array();
+
+		$links['wp:term'] = array();
+		foreach ( $response->data['terms'] as $term_id ) {
+			$term = get_term( (int) $term_id );
+			if ( empty( $term ) || is_wp_error( $term ) ) {
+				continue;
+			}
+			$obj = get_taxonomy( $term->taxonomy );
+			if ( empty( $obj ) ) {
+				continue;
+			}
+
+			$rest_base = ! empty( $obj->rest_base ) ? $obj->rest_base : $obj->name;
+			$base = sprintf( '/wp/v2/%s', $rest_base );
+
+			$links['wp:term'][] = array(
+				'href'       => rest_url( trailingslashit( $base ) . $term_id ),
+				'embeddable' => true,
+				'taxonomy'  => $term->taxonomy,
+			);
+		}
+		return $links;
 	}
 
 	/**
