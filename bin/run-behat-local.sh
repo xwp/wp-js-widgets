92c90291bc82439bae44e754aa9711847fbbe3b8
Weston Ruter <weston@xwp.co>
2017-02-26 22:42:12 -0800

    Define wd_host in environment variable

--- bin/run-behat-local.sh
+++ bin/run-behat-local.sh
@@ -9,7 +9,9 @@ pjspid=$!
 sleep 3 # Wait for it to be ready.
 echo $pjspid
 
-export ACCEPTANCE_PANTHEON_SITE_USERNAME=admin ACCEPTANCE_PANTHEON_SITE_PASSWORD=admin BEHAT_PARAMS='{"extensions": {"Behat\\MinkExtension": {"base_url": "http://src.wordpress-develop.dev"} }}'
+export ACCEPTANCE_PANTHEON_SITE_USERNAME=admin
+export ACCEPTANCE_PANTHEON_SITE_PASSWORD=admin
+export BEHAT_PARAMS='{"extensions": {"Behat\\MinkExtension": {"base_url": "http://src.wordpress-develop.dev", "selenium2":{ "wd_host": "http://localhost:8643/wd/hub" }} }}'
 
 vendor/bin/behat -c tests/behat/behat.yml
 
